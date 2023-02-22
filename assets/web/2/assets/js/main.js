01aad35fb9f76562e8cfdb0489e21c42cf52ea11
Yonesky Simamora <yonesskysimamora@gmail.com>
2023-02-22 11:45:25 +0700

    first

--- assets/web/2/assets/js/main.js
+++ assets/web/2/assets/js/main.js
@@ -1,245 +1,288 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () => {
-  const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-  this.scrollY >= 50
-    ? header.classList.add("scroll-header")
-    : header.classList.remove("scroll-header");
-};
-window.addEventListener("scroll", scrollHeader);
+/*--------------------------
+    Project Name: DTE
+    Version: 1.0
+    Author: DTE
+    Devloped by: DTE
+    Relase Date: -
+---------------------------*/
+/*---------------------------
+      Table of Contents
+    --------------------
+    01- Pre Loading
+    02- Mobile Menu
+    03- Sticky Navbar
+    04- Search Popup 
+    05- Scroll Top Button
+    06-  Scroll Top Button
+    07- Set Background-img to section 
+    08- Add active class to accordions
+    09- Load More Items
+    10- Slick Carousel
+    11- Popup Video
+    12- CounterUp
+    13- NiceSelect Plugin
+     
+ ----------------------------*/
 
-/*=============== SWIPER POPULAR ===============*/
-var swiperPopular = new Swiper(".popular__container", {
-  spaceBetweem: 32,
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  loop: true,
+$(function () {
+  "use strict";
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+  // Global variables
+  var $win = $(window);
 
-/*=============== SWIPER SERVICES ===============*/
-var swiperPopular = new Swiper(".services__container", {
-  spaceBetweem: 32,
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  loop: true,
+  /*==========  Pre Loading   ==========*/
+  setTimeout(function () {
+    $(".preloader").remove();
+  }, 2000);
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+  /*==========   Mobile Menu   ==========*/
+  var $navToggler = $(".navbar-toggler");
+  $navToggler.on("click", function () {
+    $(this).toggleClass("actived");
+  });
+  $navToggler.on("click", function () {
+    $(".navbar-collapse").toggleClass("menu-opened");
+  });
 
-/*=============== VALUE ACCORDION ===============*/
-const accordionItems = document.querySelectorAll(".value__accordion-item, .services__accordion-item");
-
-accordionItems.forEach((item) => {
-  const accordionHeader = item.querySelector(".value__accordion-header, .services__accordion-header");
-
-  accordionHeader.addEventListener("click", () => {
-    const openItem = document.querySelector(".accordion-open");
-
-    toggleItem(item);
-
-    if (openItem && openItem !== item) {
-      toggleItem(openItem);
+  /*==========   Sticky Navbar   ==========*/
+  $win.on("scroll", function () {
+    if ($win.width() >= 992) {
+      var $navbar = $(".navbar");
+      if ($win.scrollTop() > 50) {
+        $navbar.addClass("is-sticky");
+      } else {
+        $navbar.removeClass("is-sticky");
+      }
     }
   });
-});
 
-const toggleItem = (item) => {
-  const accordionContent = item.querySelector(".value__accordion-content");
+  /*==========  Search Popup  ==========*/
+  $(".action-btn__search").on("click", function (e) {
+    e.preventDefault();
+    $(".search-popup")
+      .toggleClass("active", "inActive")
+      .removeClass("inActive");
+  });
+  // Close Module Search
+  $(".search-popup__close").on("click", function () {
+    $(".search-popup").removeClass("active").addClass("inActive");
+  });
 
-  if (item.classList.contains("accordion-open")) {
-    accordionContent.removeAttribute("style");
-    item.classList.remove("accordion-open");
-  } else {
-    accordionContent.style.height = accordionContent.scrollHeight + "px";
-    item.classList.add("accordion-open");
-  }
-};
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll("section[id]");
-
-const scrollActive = () => {
-  const scrollY = window.pageYOffset;
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute("id"),
-      sectionsClass = document.querySelector(
-        ".nav__menu a[href*=" + sectionId + "]"
-      );
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      sectionsClass.classList.add("active-link");
+  /*==========   Scroll Top Button   ==========*/
+  var $scrollTopBtn = $("#scrollTopBtn");
+  // Show Scroll Top Button
+  $win.on("scroll", function () {
+    if ($(this).scrollTop() > 700) {
+      $scrollTopBtn.addClass("actived");
     } else {
-      sectionsClass.classList.remove("active-link");
+      $scrollTopBtn.removeClass("actived");
     }
   });
-};
-window.addEventListener("scroll", scrollActive);
+  // Animate Body after Clicking on Scroll Top Button
+  $scrollTopBtn.on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
 
-/*=============== SHOW SCROLL UP ===============*/
-const scrollUp = () => {
-  const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-  this.scrollY >= 350
-    ? scrollUp.classList.add("show-scroll")
-    : scrollUp.classList.remove("show-scroll");
-};
-window.addEventListener("scroll", scrollUp);
+  /*==========   Set Background-img to section   ==========*/
+  $(".bg-img").each(function () {
+    var imgSrc = $(this).children("img").attr("src");
+    $(this)
+      .parent()
+      .css({
+        "background-image": "url(" + imgSrc + ")",
+        "background-size": "cover",
+        "background-position": "center",
+      });
+    $(this).parent().addClass("bg-img");
+    if ($(this).hasClass("background-size-auto")) {
+      $(this).parent().addClass("background-size-auto");
+    }
+    $(this).remove();
+  });
 
-/*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "bx-sun";
+  /*==========   Add active class to accordions   ==========*/
+  $(".accordion__item-header").on("click", function () {
+    $(this).parent(".accordion-item").addClass("opened");
+    $(this).parent(".accordion-item").siblings().removeClass("opened");
+  });
+  $(".accordion__item-title").on("click", function (e) {
+    e.preventDefault();
+  });
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
-    iconTheme
-  );
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
-});
-
-// /*=============== SCROLL REVEAL ANIMATION ===============*/
-// const sr = ScrollReveal({
-//   origin: "top",
-//   distance: "60px",
-//   duration: 2500,
-//   delay: "400",
-//   // reset: true
-// });
-
-// sr.reveal(
-//   `.home__title, .popular__container, .subscribe__container, .footer__container`
-// );
-// sr.reveal(`.home__description, .footer__info`, { delay: 500 });
-// sr.reveal(`.home__search`, { delay: 600 });
-// sr.reveal(`.home__value`, { delay: 700 });
-// sr.reveal(`.home__images`, { delay: 800, origin: "bottom" });
-// sr.reveal(`.logos__img`, { interval: 100 });
-// sr.reveal(`.value__images, .contact__content`, { origin: "left" });
-// sr.reveal(`.value__content, .contact__images`, { origin: "right" });
-
-// /* ---------------------------------------------- /*
-//          * Youtube video background
-// /* ---------------------------------------------- */
-
-// $(function () {
-//   $(".video-player").mb_YTPlayer();
-// });
-
-// $("#video-play").click(function (event) {
-//   event.preventDefault();
-//   if ($(this).hasClass("fa-play")) {
-//     $(".video-player").playYTP();
-//   } else {
-//     $(".video-player").pauseYTP();
-//   }
-//   $(this).toggleClass("fa-play fa-pause");
-//   return false;
-// });
-
-// $("#video-volume").click(function (event) {
-//   event.preventDefault();
-//   if ($(this).hasClass("fa-volume-off")) {
-//     $(".video-player").YTPUnmute();
-//   } else {
-//     $(".video-player").YTPMute();
-//   }
-//   $(this).toggleClass("fa-volume-off fa-volume-up");
-//   return false;
-// });
-
-/*==========   counterUp  ==========*/
-$(".counter").counterUp({
-  delay: 10,
-  time: 4000,
-});
-
-
-/*==========   Slick Carousel ==========*/
-$(".slick-carousel").slick();
-
-$(".slider-with-navs").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: false,
-  dots: false,
-  asNavFor: ".slider-nav",
-});
-
-$(".slider-nav").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: ".slider-with-navs",
-  dots: false,
-  arrows: true,
-  centerMode: true,
-  focusOnSelect: true,
-  centerPadding: "15px",
-});
-
-/*----------  slick Carousel with Filter  ----------*/
-$("#slick-filter-buttons .nav__link").on("click", function (e) {
-  e.preventDefault();
-  $(this).addClass("active").siblings().removeClass("active");
-  var key = "." + $(this).data("value");
-
-  $("#filter-carousel").slick("slickUnfilter");
-  $("#filter-carousel").slick("slickFilter", key).slick("refresh");
-  $("#filter-carousel").slick("slickGoTo", 0);
-});
-
-
-/*==========   Set Background-img to section   ==========*/
-$(".bg-img").each(function () {
-  var imgSrc = $(this).children("img").attr("src");
-  $(this)
-    .parent()
-    .css({
-      "background-image": "url(" + imgSrc + ")",
-      "background-size": "cover",
-      "background-position": "center",
+  /*==========   Load More Items  ==========*/
+  function loadMore(loadMoreBtn, loadedItem) {
+    $(loadMoreBtn).on("click", function (e) {
+      e.preventDefault();
+      $(this).fadeOut();
+      $(loadedItem).fadeIn();
     });
-  $(this).parent().addClass("bg-img");
-  if ($(this).hasClass("background-size-auto")) {
-    $(this).parent().addClass("background-size-auto");
   }
-  $(this).remove();
+
+  loadMore(".loadMoreportfolio", ".portfolio-hidden > .portfolio-item");
+
+  /*==========  Contact Form validation  ==========*/
+  var contactForm = $("#contactForm"),
+    contactResult = $(".contact-result");
+  contactForm.validate({
+    debug: false,
+    submitHandler: function (contactForm) {
+      $(contactResult, contactForm).html("Please Wait...");
+      $.ajax({
+        type: "POST",
+        url: "assets/php/contact.php",
+        data: $(contactForm).serialize(),
+        timeout: 20000,
+        success: function (msg) {
+          $(contactResult, contactForm)
+            .html(
+              '<div class="alert alert-success" role="alert"><strong>Thank you. We will contact you shortly.</strong></div>'
+            )
+            .delay(3000)
+            .fadeOut(2000);
+        },
+        error: $(".thanks").show(),
+      });
+      return false;
+    },
+  });
+
+  /*==========   Slick Carousel ==========*/
+  $(".slick-carousel").slick();
+
+  /*==========  Popup Video  ==========*/
+  $(".popup-video").magnificPopup({
+    mainClass: "mfp-fade",
+    removalDelay: 0,
+    preloader: false,
+    fixedContentPos: false,
+    type: "iframe",
+    iframe: {
+      markup:
+        '<div class="mfp-iframe-scaler">' +
+        '<div class="mfp-close"></div>' +
+        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        "</div>",
+      patterns: {
+        youtube: {
+          index: "youtube.com/",
+          id: "v=",
+          src: "//www.youtube.com/embed/%id%?autoplay=1",
+        },
+      },
+      srcAction: "iframe_src",
+    },
+  });
+  $(".popup-gallery-item").magnificPopup({
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1],
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
+  });
+
+  /*==========   counterUp  ==========*/
+  $(".counter").counterUp({
+    delay: 10,
+    time: 4000,
+  });
+
+  /*==========  NiceSelect Plugin  ==========*/
+  $("select").niceSelect();
+
+  /*==========   portfolio Filtering and Sorting  ==========*/
+  $("#filtered-items-wrap").mixItUp();
+  $(".portfolio-filter li a").on("click", function (e) {
+    e.preventDefault();
+  });
+
+  /*----------  slick Carousel with Filter  ----------*/
+  $("#slick-filter-buttons .nav__link").on("click", function (e) {
+    e.preventDefault();
+    $(this).addClass("active").siblings().removeClass("active");
+    var key = "." + $(this).data("value");
+
+    $("#filter-carousel").slick("slickUnfilter");
+    $("#filter-carousel").slick("slickFilter", key).slick("refresh");
+    $("#filter-carousel").slick("slickGoTo", 0);
+  });
+});
+
+/* ---------------------------------------------- /*
+         * Youtube video background
+/* ---------------------------------------------- */
+
+$(function () {
+  $(".video-player").mb_YTPlayer();
+});
+
+$("#video-play").click(function (event) {
+  event.preventDefault();
+  if ($(this).hasClass("fa-play")) {
+    $(".video-player").playYTP();
+  } else {
+    $(".video-player").pauseYTP();
+  }
+  $(this).toggleClass("fa-play fa-pause");
+  return false;
+});
+
+$("#video-volume").click(function (event) {
+  event.preventDefault();
+  if ($(this).hasClass("fa-volume-off")) {
+    $(".video-player").YTPUnmute();
+  } else {
+    $(".video-player").YTPMute();
+  }
+  $(this).toggleClass("fa-volume-off fa-volume-up");
+  return false;
+});
+
+/* ---------------------------------------------- /*
+         * Set sections backgrounds
+/* ---------------------------------------------- */
+
+var module = $(".home-section, .module, .module-small, .side-image");
+module.each(function (i) {
+  if ($(this).attr("data-background")) {
+    $(this).css(
+      "background-image",
+      "url(" + $(this).attr("data-background") + ")"
+    );
+  }
+});
+
+/* ---------------------------------------------- /*
+         * Share Button
+/* ---------------------------------------------- */
+
+const shareBtn = document.getElementById("shareBtn");
+
+shareBtn.addEventListener("click", (event) => {
+  // Check for Web Share api support
+  if (navigator.share) {
+    //Browser supports native share api
+    navigator.share({
+        text: "Please read this great article: ",
+        url: "https://www.google.com/",
+      }).then(() => {
+        console.log("Thanks for sharing!");
+      })
+      .catch((err) => console.error(err));
+  } else {
+    // Fallback
+    alert(
+      "The current browser does not support the share function. Please, manually share the link"
+    );
+  }
 });
